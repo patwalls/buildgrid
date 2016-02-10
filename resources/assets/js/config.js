7966398ea1c028d8ce67c40ab56be39cb15960e1
martemorfosis <martemorfosis@gmail.com>
2016-02-09 19:25:54 -0600

    Added inputmask global. Removed jquery-modal reference because it collides with the Bootstrap modal namespace.

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -2,7 +2,7 @@
 
 global.$ = global.jQuery = require('jquery');       // jQuery is expected to be found tied to the window object.
 global.router    = require('jquery-router-plugin');
-global.modal     = require('jquery-modal');
+global.inputmask = require('jquery.inputmask');
 global.toastr    = require('toastr');
 global.dropzone  = require('dropzone');
 global.jscroll   = require('jscroll');
@@ -16,3 +16,6 @@ toastr.options.closeButton = true;
 toastr.options.progressBar = true;
 toastr.options.timeOut = 3000;
 
+// Dropzone defaults
+
+dropzone.autoDiscover = false;      // Prevents Dropzone from automatically instantiating drop zones.
