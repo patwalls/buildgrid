79ffcc54915c9fdb61e4b272758f456aad921829
martemorfosis <martemorfosis@gmail.com>
2016-02-09 19:17:51 -0600

    Updated routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,34 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
 
 Route::group(['middleware' => 'web'], function () {
+
+    // Site's main page
+
+    Route::get('/',      function () { return view('welcome'); });
+
+
+    // Auth
+
     Route::auth();
-    Route::get('/home', 'HomeController@index');
-    Route::post('file-upload', 'HomeController@fileUpload');
+    Route::get('/login', function () { return view('welcome'); });
+
+
+
+    // User
+
+    Route::get('/home', 'ProjectController@index');
+    Route::get('/create_project', 'ProjectController@create');
+    Route::post('/create_project', 'ProjectController@saveNewProject');
+
+
+
+    // Admin
+
+
 });
