5b74e8caeab47d1040ad40aa6b337da475bc5b85
martemorfosis <martemorfosis@gmail.com>
2016-02-09 19:29:14 -0600

    Make registration form to match the design's blueprint

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,82 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {!! csrf_field() !!}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Name</label>
+    <div class="container">
 
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+        <div class="row main-row">
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+            <div class="col-md-4 col-md-offset-1">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i>Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Sign Up</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores cupiditate doloremque fuga.
+                        </p>
+                    </div>
                 </div>
+
+
+                <div class="row">
+
+                    <form role="form" method="POST" action="{{ url('/register') }}">
+
+                        <div class="col-md-12">
+                            <label for="first_name">Name</label>
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <input type="text" class="form-control" name="first_name" placeholder="First">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <input type="text" class="form-control" name="last_name" placeholder="Last">
+                        </div>
+
+
+                        <div class="form-group col-md-12">
+                            <label for="email">Email address</label>
+                            <input type="email" class="form-control" name="email" placeholder="Email">
+                        </div>
+
+                        <div class="form-group col-md-12">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" name="password" placeholder="Password">
+                            {!! csrf_field() !!}
+                        </div>
+
+
+                        <div class="form-group col-md-6">
+                            <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
+                        </div>
+
+                    </form>
+
+                </div>
+
+                <div class="row">
+                    <p class="text-center">
+                        Or
+                    </p>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <a class="btn btn-block btn-primary bg-google-plus bd-google-plus ho-google-plus" href="{{ url('/password/reset') }}">Sign Up with Google+</a>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="btn btn-block btn-primary bg-linkedin bd-linkedin ho-linkedin" href="{{ url('/password/reset') }}">Sign Up with LinkedIn</a>
+                    </div>
+                </div>
+
             </div>
+
+            <div class="col-md-6 col-md-offset-1 hidden-sm hidden-xs">
+                <img src="{{ asset('images/brand/BG-logo.jpg') }}" alt="BuildGrid Logo" class="center-block logo"/>
+            </div>
+
         </div>
     </div>
-</div>
+
 @endsection
