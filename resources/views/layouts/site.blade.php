09d9e48e84f5ee98241a893c23d1e79e145f204e
martemorfosis <martemorfosis@gmail.com>
2016-02-12 15:34:43 -0600

    Separated App and Site layouts

---
+++ resources/views/layouts/site.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>BuildGrid</title>
+
+    <!-- Fonts -->
+    <link href="" rel='stylesheet' type='text/css'>
+
+    <!-- Styles -->
+    <link href="{{  elixir('css/site.css') }}" rel="stylesheet">
+
+</head>
+<body>
+
+
+    @yield('content')
+
+    <!-- JavaScripts -->
+
+    <script src="{{ elixir('js/site.js') }}"></script>
+
+
+</body>
+</html>
