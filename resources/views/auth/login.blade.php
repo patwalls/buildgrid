d88d264da086ec18f7660ae038f580ade053c4e1
Eduardo Zarate <eduardo@bbfour.com>
2016-02-17 16:31:10 -0600

    Minor changes on UI and link to LinkedIn login.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,16 +2,15 @@
 
 @section('content')
 
-    <div class="container">
-
-        <div class="row main-row">
-			<form role="form" method="POST" action="{{ url('/login') }}" class="login-form">
+<div class="container">
+    <div class="row main-row">
+	    <form role="form" method="POST" action="{{ url('/login') }}" class="login-form">
             <div class="col-md-6 col-md-offset-3 col-xs-10 col-xs-offset-1">
 
                 <div class="row">
                     <div class="col-md-12 text-center">
-                        <h2>Login to BuildGrid</h2>
-                        <p>
+                        <span class="header">Login to <strong>BuildGrid</strong></span>
+                        <p class="body-copy-2">
                             Login to BuildGrid to access your account
                         </p>
                         
@@ -46,7 +45,7 @@
                             <button type="submit" class="btn btn-block standard-blue-button">Log in</button>
                         </div>                	
                 </div>
-				</form>
+				
 				
                 <div class="row form-group">
                 	<div class="col-md-12 col-xs-12">
@@ -55,17 +54,20 @@
 	                    </p>
 					</div>
                 </div>
-
-                <div class="row">
-                    <div class="col-md-6 col-xs-6 form-group">
+            <div class="row">
+                    <div class="col-md-5 col-md-offset-1 col-xs-5 col-xs-offset-1 form-group">
                         <a class="btn btn-block standard-blue-button" href="{{ url('/password/reset') }}">Google+</a>
                     </div>
-                    <div class="col-md-6 col-xs-6 form-group">
-                        <a class="btn btn-block standard-blue-button" href="{{ url('/password/reset') }}">LinkedIn</a>
+                    <div class="col-md-5 col-xs-5 form-group">
+                        <a class="btn btn-block standard-blue-button" href="{{ route('login.linkedin') }}">LinkedIn</a>
                     </div>
-                </div>
             </div>
+            <div class="row">
+                <div class="col-md-12">&nbsp;</div>
+            </div>
+            </form>
         </div>
     </div>
+</div>
 
 @endsection
