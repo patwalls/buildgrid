33499e7a3012eae801fcc46fff5eb72d264b57fd
martemorfosis <martemorfosis@gmail.com>
2016-02-29 14:50:17 -0600

    Added right "arrow" indicator

--- resources/views/partials/marketing-index/hero-wrap.blade.php
+++ resources/views/partials/marketing-index/hero-wrap.blade.php
@@ -3,6 +3,6 @@
     <h2>Welcome to<span> Build</span>Grid</h2>
     <div class="b1">The materials purchasing marketplace you</div>
     <div class="b1">always wanted, <span class="b2">but didn’t expect to get.</span></div>
-    <a href="#"><span class="b1">Get Started</span><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a>
+    <a href="#"><span class="b1">Get Started ></span><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a>
   </div>
 </div>
