eada12f9dd34e64f6dc11270e4f652512ba28622
martemorfosis <martemorfosis@gmail.com>
2016-03-21 14:23:23 -0600

    Detect file MIME Type for assembling the dowload headers correctly.

--- app/Http/Controllers/BomController.php
+++ app/Http/Controllers/BomController.php
@@ -3,26 +3,100 @@
 namespace BuildGrid\Http\Controllers;
 
 use BuildGrid\Bom;
+use BuildGrid\InvitedSupplier;
+use BuildGrid\Repositories\BomRepository;
 use Illuminate\Http\Request;
 use BuildGrid\Http\Requests;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\View;
 
-class BomController extends Controller
-{
+class BomController extends Controller {
+
+
+    /**
+     * BomController constructor.
+     * @param BomRepository $bomRepository
+     */
+    public function __construct(BomRepository $bomRepository)
+    {
+        $this->bomRepository = $bomRepository;
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
     public function bomFileUpload(Request $request)
     {
         $bom = Bom::findOrFail($request->bom_id);
 
-        if($bom->project->user_id !== 1234 ){
+        if($bom->project->user_id !== \Auth::id() ){
             return response('Unauthorized', 403);
         }
 
         $file = $request->file('file');
-        $file_storage_path = \Auth::id() . '/' . $bom->project->id . ' - ' . $bom->project->name . '/' . $bom->filename;
 
-        Storage::disk('dropbox')->put($file_storage_path, file_get_contents($file));
+        if( $this->bomRepository->storeBomFile($bom, $file) == true){
+            return response('OK');
+        }
 
-        return response('OK');
+        return response('We could not store the document', 500);
 
     }
+
+
+    public function bomResponseUpload(Request $request)
+    {
+        dd( $request->all() );
+    }
+
+
+
+    /**
+     * @param $hashid
+     * @return mixed
+     */
+    public function displayBomForSupplier($hashid)
+    {
+
+        $supplier = InvitedSupplier::where('hashid', $hashid)->first();
+
+        if(! $supplier){
+            return response('Not Found', 404);
+        }
+
+        return View::make('supplier.bom_supplier_view', compact(['supplier']));
+
+    }
+
+
+
+    /**
+     * @param $bom_id
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function fileDownload($hashid)
+    {
+        $invited_supplier = InvitedSupplier::where('hashid', $hashid)->first();
+
+        if (! $invited_supplier ) {
+            return response('Not Found', 404);
+        }
+
+        $bom = Bom::findOrFail($invited_supplier->bom_id);
+
+        $file = $this->bomRepository->retrieveBomFile($bom);
+
+        if (! $file ) {
+            return response('Not Found', 404);
+        }
+
+        $headers = [
+            'Content-Type' => $file['mimeType'],
+            'Content-Disposition' => 'attachment; filename="' . $bom->filename .'"'
+        ];
+
+        return response($file['contents'], 200, $headers);
+    }
+
 }
