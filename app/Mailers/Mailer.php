177297de5a7e7e491f229449239bc02d4c1f73b1
martemorfosis <martemorfosis@gmail.com>
2016-03-21 14:23:49 -0600

    Created Base Mailer Class

---
+++ app/Mailers/Mailer.php
@@ -0,0 +1,14 @@
+<?php namespace BuildGrid\Mailers;
+
+abstract class Mailer {
+
+
+    public function sendMail($emailAddress, $subject, $view, $data = []){
+        Mail::send($view, $data, function($message) use ($emailAddress, $subject) {
+            $message->to($emailAddress)
+                ->subject($subject);
+        });
+    }
+
+
+}
