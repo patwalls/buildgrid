8866318f5584405048fc4780724ad4b32e1a19ad
martemorfosis <martemorfosis@gmail.com>
2016-03-21 14:23:39 -0600

    Created Invited SupplierMailer

---
+++ app/Mailers/InvitedSupplierMailer.php
@@ -0,0 +1,21 @@
+<?php namespace BuildGrid\Mailers;
+
+
+class InvitedSupplierMailer extends Mailer {
+
+
+    public function sendBomInvitationForNewProject($bom_id)
+    {
+        // Sends invitations for a newly created project
+
+    }
+
+
+    public function sendBomInvitation($invited_supplier_id, $bom_ud)
+    {
+        // Sends an invidual Bom invitation to a supplier
+
+    }
+
+
+}
