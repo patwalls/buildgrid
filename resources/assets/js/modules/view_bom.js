35f7b9938c22301ff792fb5f6e80008712adb43b
martemorfosis <martemorfosis@gmail.com>
2016-04-21 07:56:41 -0500

    Added js module for view bom

---
+++ resources/assets/js/modules/view_bom.js
@@ -0,0 +1,28 @@
+module.exports = (data) => {
+
+    "use strict";
+
+    $('.js-resend-email').click(function(){
+        // var $_this = $(this);
+        var $btn = $( this ).button('loading');
+        var emailTargetUrl = $('[data-href]').data( "href");
+        // $_this.prop('disabled', true);
+        $.ajax({
+            url: emailTargetUrl,
+            context: document.body
+        }).done(function() {
+            $btn.button('reset')
+            $('#bom-notify-success').fadeIn( 400 ).delay( 1600 ).fadeOut( 400 );;
+        });
+    });
+
+    $('.add-supplier-to-bom').click((e) => {
+        e.preventDefault();
+        var group_count = $( '.supplier-item-wrap' ).length;
+        $( '.supplier-item-wrap' ).last().after(`<div class="row supplier-item-wrap"><div class="form-group col-md-6"><input type="text" class="form-control" name="supplier[${group_count + 1}][name]" placeholder="Supplier Name"></div><div class="form-group col-md-6"><input type="text" class="form-control" name="supplier[${group_count + 1}][email]" placeholder="Supplier Email"></div></div>`);
+    });
+
+
+    pdfObject.embed($('#pdf-preview').data('document-url'), '#pdf-preview', { height: "320px" });
+
+};
