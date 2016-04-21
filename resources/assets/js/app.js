6d9cc0457de697553e2d2cbeda1be5d973ec9fe6
martemorfosis <martemorfosis@gmail.com>
2016-04-21 07:57:10 -0500

    Moved code from here to view_bom.js module

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,23 +2,3 @@
 
 require('./config');
 require('./router');
-
-  
-$('.js-resend-email').click(function(){
-
-  var $_this = $(this);
-  var $btn = $( this ).button('loading');
-  var emailTargetUrl = $('[data-href]').data( "href");
-  // $_this.prop('disabled', true);
-  $.ajax({
-    url: emailTargetUrl,
-    context: document.body
-  }).done(function() {
-    $btn.button('reset')
-    $('#bom-notify-success').fadeIn( 400 ).delay( 1600 ).fadeOut( 400 );;
-  });
-});
-
-$('.add-supplier-to-bom').click(function(){
-  $( '.supplier-item-wrap' ).last().after('<div class="row supplier-item-wrap"><div class="form-group col-md-6"><input type="text" class="form-control" name="supplier[1][name]" placeholder="Supplier Name"></div><div class="form-group col-md-6"><input type="text" class="form-control" name="supplier[1][email]" placeholder="Supplier Email"></div></div>');
-});
