50f3e7c7c132ca49ea965ee16dfa1f61ed72f223
Scott Harrington <scott@kingtidecreative.com>
2016-05-10 17:21:58 -0700

    Resolved merge conflicts

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,18 +3,6 @@
 require('./config');
 require('./router');
 
-// These need to be moved into their own modules -SH
-$(document).ready(function(){
-
-  // Aligns Header / Footer on small screens vertically  
-  var shortWindow = $( document ).height();
-
-  if ( shortWindow <= 900 ){
-    $('#app-layout').addClass('veritcal-separator-wrap');
-  }
-
-  // Archive button on View BOM
-
   $('[data-toggle="popover"]').popover({
     html: true,
     title: 'Are you sure?',
