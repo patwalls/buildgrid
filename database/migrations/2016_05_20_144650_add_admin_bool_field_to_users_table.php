afca651396f6d041e0dc2e38193141a78a4a7427
martemorfosis <marte@kingtidecreative.com>
2016-05-20 11:57:05 -0500

    Renamed isAdmin column to is_admin for DB naming consistency

--- database/migrations/2016_05_20_144650_add_admin_bool_field_to_users_table.php
+++ database/migrations/2016_05_20_144650_add_admin_bool_field_to_users_table.php
@@ -13,7 +13,7 @@ class AddAdminBoolFieldToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function(Blueprint $table){
-            $table->boolean('isAdmin');
+            $table->boolean('is_admin');
         });
     }
 
@@ -25,7 +25,7 @@ class AddAdminBoolFieldToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function(Blueprint $table){
-            $table->dropColumn('isAdmin');
+            $table->dropColumn('is_admin');
         });
     }
 }
