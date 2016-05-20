621cf4e118301f94126f9c6b995a28d0765e345e
Scott Harrington <scott@kingtidecreative.com>
2016-05-20 11:07:48 -0700

    Finished restyling of create project and add BOM

---
+++ resources/views/partials/add-project-header.blade.php
@@ -0,0 +1,14 @@
+<div class="app-current-proj-outer-wrap">
+    <div class="container" id="app-current-proj-wrap">
+        <div class="row">
+            <div class="col-md-12 inner-wrap">
+                <div class="col-md-6 item-wrap">
+                    <span class="b2">Create Project</span>
+                </div>
+                <div class="col-md-6 item-wrap">
+                    {{-- <a href="{{ route('getCreateProject') }}"><button class="btn btn-success new-proj-btn">New Project</button></a> --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
