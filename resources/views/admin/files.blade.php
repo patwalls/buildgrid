63f54ee3ec32dad2e2b2a1e6e2f3c1713c37ebc9
martemorfosis <marte@kingtidecreative.com>
2016-06-07 17:22:39 -0500

    Removed remaining Files view from Admin

--- resources/views/admin/files.blade.php
+++
@@ -1,5 +0,0 @@
-<a style="display: block; font-weight: bold; font-size: 18px;" href="https://www.dropbox.com/sh/qb4lq6c5mvcle65/AABIPpU_7yHBxkKeXaUQmOaPa?dl=0" target="_blank">View these files in Dropbox</a>
-
-<iframe src="/files" frameborder="0" style="width: 90%; height: 600px;"></iframe>
-
-
