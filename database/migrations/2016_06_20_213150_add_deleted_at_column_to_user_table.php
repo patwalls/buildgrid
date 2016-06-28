083efd11461ba0812c0fb073159269db45e6781c
martemorfosis <marte@kingtidecreative.com>
2016-06-28 16:14:26 -0500

    Add deleted_at columns to several tables

--- database/migrations/2016_06_20_213150_add_deleted_at_column_to_user_table.php
+++ database/migrations/2016_06_20_213150_add_deleted_at_column_to_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusToUser extends Migration
+class AddDeletedAtColumnToUserTable extends Migration
 {
     /**
      * Run the migrations.
