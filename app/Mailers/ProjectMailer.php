6d001cefe8eaf7be5ffcc16036a9b8ff63294a1b
martemorfosis <marte@kingtidecreative.com>
2016-07-11 18:47:33 -0500

    Added project and bom details to be added in email

--- app/Mailers/ProjectMailer.php
+++ app/Mailers/ProjectMailer.php
@@ -4,6 +4,7 @@
 namespace BuildGrid\Mailers;
 
 
+use BuildGrid\Bom;
 use BuildGrid\Project;
 use BuildGrid\User;
 
@@ -17,11 +18,14 @@ class ProjectMailer extends Mailer
     public static function sendProjectCreatedMail(Project $project)
     {
         $project_name = $project->name;
+        $bom = Bom::where('project_id', $project->id)->first();
 
         $subject = 'New project was created';
         $view = 'email.project.new_project';
         $data = [
-            'project_name' => $project_name
+            'project_name' => $project_name,
+            'bom_name' => $bom->name,
+            'project_bom_admin_url' => url('admin/boms/' . $bom->id )
         ];
 
         return AdminMailer::sendMailToAdmin($data, $subject, $view);
