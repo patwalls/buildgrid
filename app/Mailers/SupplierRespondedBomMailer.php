b223d14b6ef02e727f6d0d8b99a180fd6f3703e2
martemorfosis <marte@kingtidecreative.com>
2016-10-28 15:54:30 -0500

    Added Project Bom URL and updated copy in email.

--- app/Mailers/SupplierRespondedBomMailer.php
+++ app/Mailers/SupplierRespondedBomMailer.php
@@ -25,14 +25,15 @@ class SupplierRespondedBomMailer  extends Mailer
         $project_name           = $bomResponse->bom->project->name;
 
 
-        $subject = "A Supplier Responded your BOM";
+        $subject = "A Supplier Responded to your BOM";
         $view = 'email.bom_response.supplier_responded_bom';
 
 
         $data = [
             'purchaser_name'         => $purchaser_name,
             'purchaser_email'        => $purchaser_email,
-            'project_name'           => $project_name
+            'project_name'           => $project_name,
+            'project_url'            => link_to_route('getShowBom', $project_name , ['id' => $bomResponse->bom->id])
         ];
 
         return parent::sendMail($purchaser_email, $subject, $view, $data);
