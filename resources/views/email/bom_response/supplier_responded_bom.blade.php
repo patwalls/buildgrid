8bb6344e3b2dba220c6d07f2b72fa597d2e1bb58
Tune <Reidcrawford55@gmail.com>
2016-12-20 15:04:51 -0800

    reverted the view for the supplier responded to bom email to its original form

--- resources/views/email/bom_response/supplier_responded_bom.blade.php
+++ resources/views/email/bom_response/supplier_responded_bom.blade.php
@@ -7,11 +7,11 @@
     </p>
 
     <p>
-    <strong>A Supplier Responded To Your BOM - {{$bom_name}}</strong> for project: <strong>{{ $project_name }}</strong>. Great work! <br>
+    A supplier has responded your BOM <strong>{{$bom_name}}</strong> for project: <strong>{{ $project_name }}</strong>. Great work! <br>
     </p>
 
     <p>
-        <strong>You can view the response here: {{ $project_url }} </strong>
+        <strong>You can view the response here : {{ $project_url }} </strong>
     </p>
 
     <p>If you have any questions, please feel free to email us directly at <a href="mailto:info@buildgrid.com">info@buildgrid.com</a></p>
